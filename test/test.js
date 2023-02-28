9cd43878880e1824a9090783a8ce98d1b91ae256
Barrer4 <sandra_rb@hotmail.com>
2023-02-28 18:47:48 +0000

    commit

--- test/test.js
+++ test/test.js
@@ -20,48 +20,32 @@ function readLine() {
   return inputString[currentLine++];
 }
 
-/*
- * Complete the 'plusMinus' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
-function plusMinus(arr) {
-  // Write your code here
-
-  let positive = 0;
-  let negative = 0;
-  let zero = 0;
-  for (let i in arr) {
-    if (arr[i] > 0) {
-      positive++;
-    } else if (arr[i] < 0) {
-      negative++;
-    } else {
-      zero++;
-    }
-  }
-
-  let newArr = [(positive / arr.length).toFixed(6),
-    (negative / arr.length).toFixed(6),
-    (zero / arr.length).toFixed(6)];
-
-    for(let i in newArr){
-      console.log(newArr[i])
-    }
-}
-
 function main() {
-  const n = parseInt(readLine().trim(), 10);
-
-  const arr = readLine()
-    .replace(/\s+$/g, '')
-    .split(' ')
-    .map((arrTemp) => parseInt(arrTemp, 10));
-
-  plusMinus(arr);
+  const N = parseInt(readLine().trim(), 10);
 }
 
-let arr = [-4, 3, -9, 0, 4, 1];
-plusMinus(arr);
+/* If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird */
 
+function check(n) {
+  if (n >= 1 && n <= 100) {
+    if (n % 2 !== 0) {
+      console.log('Weird');
+    }
+    if (n % 2 === 0 && n >= 2 && n <= 5) {
+      console.log('Not Weird');
+    }
+    if (n % 2 === 0 && n >= 6 && n <= 20) {
+      console.log('Weird');
+    }
+    if (n % 2 === 0 && 20 < n) {
+      console.log('Not Weird');
+    }
+  }else{
+    console.log('Weird')
+  }
+}
+
+check(24);
